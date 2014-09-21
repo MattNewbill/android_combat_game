0063c965570ae282994ac8ad022d9fcb14a6050f
MattHamersky <hamerskymatthew@yahoo.com>
2014-09-21 16:25:17 -0700

    Changed game to fullscreen, added WAKE LOCK

--- src/combatgame/main/MainActivity.java
+++ src/combatgame/main/MainActivity.java
@@ -1,16 +1,28 @@
 package combatgame.main;
 
-
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+import android.content.Context;
 
 public class MainActivity extends Activity {
 
+	private WakeLock wakeLock;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+                                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
 		setContentView(R.layout.activity_main);
+		
+		PowerManager powerMananger = (PowerManager) getSystemService(Context.POWER_SERVICE);
 	}
 
 	@Override
@@ -18,6 +30,19 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		wakeLock.acquire();
+		
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		wakeLock.release();
 	}
 
 }
