88986dbeb15eddfaa590d3173e576b56ba0d64ec
MattHamersky <hamerskymatthew@yahoo.com>
2014-09-22 22:27:27 -0700

    Finished up the 2D graphics interface

--- src/combatgame/graphics/Graphics2D.java
+++ src/combatgame/graphics/Graphics2D.java
@@ -1,10 +1,15 @@
 package combatgame.graphics;
 
 import android.graphics.Paint;
+import android.graphics.Bitmap;
 
 public interface Graphics2D {
 
 	
 	public void drawRGB(int r, int g, int b);
 	public void drawRect(float left, float top, float right, float bottom, Paint paint);
+	public void drawBitmap(Bitmap bitmap, float left, float top, Paint paint);
+	public void drawText(String text, float x, float y, Paint paint);
+	public int getHeight();
+	public int getWidth();
 }
--- src/combatgame/graphics/GraphicsCPU.java
+++ src/combatgame/graphics/GraphicsCPU.java
@@ -21,4 +21,25 @@ public class GraphicsCPU implements Graphics2D {
 	public void drawRect(float left, float top, float right, float bottom, Paint paint) {
 		drawingCanvas.drawRect(left, top, right, bottom, paint);
 	}
+
+	@Override
+	public void drawBitmap(Bitmap bitmap, float left, float top, Paint paint) {
+		drawingCanvas.drawBitmap(bitmap, left, top, paint);
+	}
+
+	@Override
+	public void drawText(String text, float x, float y, Paint paint) {
+		drawingCanvas.drawText(text, x, y, paint);
+		
+	}
+
+	@Override
+	public int getHeight() {
+		return drawingCanvas.getHeight();
+	}
+
+	@Override
+	public int getWidth() {
+		return drawingCanvas.getWidth();
+	}
 }
