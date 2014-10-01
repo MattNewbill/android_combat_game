156ee1f38bb16420a92e8ac73fd0dac80ce87e20
MattHamersky <hamerskymatthew@yahoo.com>
2014-10-01 10:43:10 -0700

    Added button widget which allows easy to place buttons

--- src/combatgame/state/MainMenuState.java
+++ src/combatgame/state/MainMenuState.java
@@ -1,5 +1,6 @@
 package combatgame.state;
 
+import combatgame.widgets.*;
 import combatgame.util.Util;
 import combatgame.input.*;
 import combatgame.main.*;
@@ -15,13 +16,9 @@ public class MainMenuState extends State {
 
 	static final int V_BUTTON_MARGIN = 10;
 	
-	Bitmap startButton;
-	Bitmap aboutButton;
-	Bitmap exitButton;
-	
-	int startButtonX, startButtonY;
-	int aboutButtonX, aboutButtonY;
-	int exitButtonX, exitButtonY;
+	Button startButton;
+	Button aboutButton;
+	Button exitButton;
 	
 	public MainMenuState(StateManager stateManager) {
 		super(stateManager);
@@ -29,16 +26,26 @@ public class MainMenuState extends State {
 		AssetManager am = stateManager.getAssetManager();
 		//load main menu assets
 		try {
-			startButton = BitmapFactory.decodeStream(am.open("images/main_menu/start_button_original.png"));
-			aboutButton = BitmapFactory.decodeStream(am.open("images/main_menu/about_button_original.png"));
-			exitButton = BitmapFactory.decodeStream(am.open("images/main_menu/exit_button_original.png"));
+			//load images and determine button coordinates
+			Bitmap startButtonDisarmed = BitmapFactory.decodeStream(am.open("images/main_menu/start_button_original.png"));
+			Bitmap aboutButtonDisarmed = BitmapFactory.decodeStream(am.open("images/main_menu/about_button_original.png"));
+			Bitmap exitButtonDisarmed = BitmapFactory.decodeStream(am.open("images/main_menu/exit_button_original.png"));
 			
-			startButtonX = (Game.G_WIDTH / 2) - startButton.getWidth() / 2;
-			startButtonY = (int) (Game.G_HEIGHT / 2 - (startButton.getHeight() * 1.5));
-			aboutButtonX = (Game.G_WIDTH / 2) - aboutButton.getWidth() / 2;
-			aboutButtonY = (int) (Game.G_HEIGHT / 2 - (aboutButton.getHeight() * 1.5) + V_BUTTON_MARGIN + startButton.getHeight());
-			exitButtonX = (Game.G_WIDTH / 2) - exitButton.getWidth() / 2;
-			exitButtonY = (int) (Game.G_HEIGHT / 2 - (exitButton.getHeight() * 1.5) + (V_BUTTON_MARGIN * 2) + startButton.getHeight() + aboutButton.getHeight());
+			Bitmap startButtonArmed = BitmapFactory.decodeStream(am.open("images/main_menu/start_button_original_armed.png"));
+			Bitmap aboutButtonArmed = BitmapFactory.decodeStream(am.open("images/main_menu/about_button_original_armed.png"));
+			Bitmap exitButtonArmed = BitmapFactory.decodeStream(am.open("images/main_menu/exit_button_original_armed.png"));
+			
+			int startButtonX = (Game.G_WIDTH / 2) - startButtonDisarmed.getWidth() / 2;
+			int startButtonY = (int) (Game.G_HEIGHT / 2 - (startButtonDisarmed.getHeight() * 1.5));
+			int aboutButtonX = (Game.G_WIDTH / 2) - aboutButtonDisarmed.getWidth() / 2;
+			int aboutButtonY = (int) (Game.G_HEIGHT / 2 - (aboutButtonDisarmed.getHeight() * 1.5) + V_BUTTON_MARGIN + startButtonDisarmed.getHeight());
+			int exitButtonX = (Game.G_WIDTH / 2) - exitButtonDisarmed.getWidth() / 2;
+			int exitButtonY = (int) (Game.G_HEIGHT / 2 - (exitButtonDisarmed.getHeight() * 1.5) + (V_BUTTON_MARGIN * 2) + startButtonDisarmed.getHeight() + aboutButtonDisarmed.getHeight());
+			
+			//create actual buttons
+			startButton = new Button(startButtonDisarmed, startButtonArmed, startButtonX, startButtonY);
+			aboutButton = new Button(aboutButtonDisarmed, aboutButtonArmed, aboutButtonX, aboutButtonY);
+			exitButton = new Button(exitButtonDisarmed, exitButtonArmed, exitButtonX, exitButtonY);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -47,7 +54,28 @@ public class MainMenuState extends State {
 	@Override
 	public void update(float delta) {
 		List<TouchEvent> events = stateManager.getTouchHandler().getTouchEvents();
-		for(int i = 0; i < events.size(); i++) {
+		events = startButton.update(events);
+		events = aboutButton.update(events);
+		events = exitButton.update(events);
+		
+		if(startButton.state == Button.ACTIVATED) {
+			Log.i("combatgame", "start button pressed");
+			//bring up the next screen, whether that's the connection screen or straight to the game screen
+			stateManager.setState(new TestState(stateManager));
+		}
+		else if(aboutButton.state == Button.ACTIVATED) {
+			Log.i("combatgame", "about button pressed");
+			//spash screen shamelessly promoting the hard work we've all put in
+			stateManager.setState(new AboutState(stateManager));
+		}
+		else if(exitButton.state == Button.ACTIVATED) {
+			Log.i("combatgame", "exit button pressed");
+			//exit the app
+			//not sure if this is considered bad form or not....guess we'll find out
+			System.exit(0);
+		}
+		
+		/*for(int i = 0; i < events.size(); i++) {
 			if(events.get(i).type == TouchEvent.TOUCH_UP) {
 				if(Util.isInBounds(events.get(i), startButtonX, startButtonY, startButton.getWidth(), startButton.getHeight())) {
 					Log.i("combatgame", "start button pressed");
@@ -66,19 +94,19 @@ public class MainMenuState extends State {
 					System.exit(0);
 				}
 			}
-		}
+		}*/
 	}
 
 	@Override
 	public void render(Graphics2D g, float delta) {
 		//draw start button
-		g.drawBitmap(startButton, startButtonX, startButtonY, null);
+		startButton.render(g);
 		
 		//draw about button
-		g.drawBitmap(aboutButton, aboutButtonX, aboutButtonY, null);
+		aboutButton.render(g);
 		
 		//draw exit button
-		g.drawBitmap(exitButton, exitButtonX, exitButtonY, null);
+		exitButton.render(g);
 	}
 
 	@Override
@@ -93,12 +121,9 @@ public class MainMenuState extends State {
 
 	@Override
 	public void dispose() {
-		if(startButton != null)
-			startButton.recycle();
-		if(aboutButton != null)
-			aboutButton.recycle();
-		if(exitButton != null)
-			exitButton.recycle();
+		startButton.recycle();
+		aboutButton.recycle();
+		exitButton.recycle();
 	}
 
 }
---
+++ src/combatgame/widgets/Button.java
@@ -0,0 +1,82 @@
+package combatgame.widgets;
+
+import combatgame.util.Util;
+import combatgame.graphics.*;
+import combatgame.input.*;
+import java.util.List;
+import java.util.ArrayList;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.util.Log;
+
+public class Button {
+
+	public static final int DISARMED = 0;
+	public static final int ARMED = 1;
+	public static final int ACTIVATED = 2;
+	public int state = 0;
+	
+	private Bitmap armedImage;
+	private Bitmap disarmedImage;
+	private int x, y;
+	
+	public Button(Bitmap disarmedImage, Bitmap armedImage, int x, int y) {
+		//disarmed image cannot be null
+		if(disarmedImage == null)
+			throw new IllegalArgumentException("disarmed image cannot be null");
+		//if the armed image is null then we use the disarmed image instead
+		if(armedImage == null) 
+			this.armedImage = disarmedImage;
+		else
+			this.armedImage = armedImage;
+		this.armedImage = armedImage;
+		this.disarmedImage = disarmedImage;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Point getCoordinates() {
+		return new Point(x, y);
+	}
+	
+	public List<TouchEvent> update(List<TouchEvent> events) {
+		List<TouchEvent> unusedEvents = new ArrayList<TouchEvent>();
+		for(int i = 0; i < events.size(); i++) {
+			if(events.get(i).type == TouchEvent.TOUCH_DOWN) {
+				if(Util.isInBounds(events.get(i), x, y, disarmedImage.getWidth(), disarmedImage.getHeight())) {
+					state = ARMED;
+				}
+				else {
+					unusedEvents.add(events.get(i));
+				}
+			}
+			else if(events.get(i).type == TouchEvent.TOUCH_UP) {
+				if(Util.isInBounds(events.get(i), x, y, disarmedImage.getWidth(), disarmedImage.getHeight())) {
+					state = ACTIVATED;
+				}
+				else {
+					state = DISARMED;
+					unusedEvents.add(events.get(i));
+				}
+			}
+			else {
+				unusedEvents.add(events.get(i));
+			}
+		}
+		return unusedEvents;
+	}
+	
+	public void render(Graphics2D g) {
+		if(state == ARMED)
+			g.drawBitmap(armedImage, x, y, null);
+		else
+			g.drawBitmap(disarmedImage, x, y, null);
+	}
+	
+	public void recycle() {
+		disarmedImage.recycle();
+		if(armedImage != disarmedImage && armedImage != null) {
+			armedImage.recycle();
+		}
+	}
+}
