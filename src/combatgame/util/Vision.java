be3d74cf17b1fb98f50b3871f1af078ca747edfe
maxx9 <maxxzero@ymail.com>
2014-10-16 14:17:46 -0700

    fixed up vision a bit

--- src/combatgame/util/Vision.java
+++ src/combatgame/util/Vision.java
@@ -18,19 +18,20 @@ import android.util.Log;
 
 public class Vision
 {
-	private static GPoint loc, temp;
+	private static GPoint loc;
 	private static int view;
 	private static int width;
 	private static int height;
+//	private static char direction;
 	private static int direction;
 	
 	private static Map MAP;
 	private static Unit UNIT;
 
-	private static int px, nx, py, ny;
+	private static int left, right, top, bottom;
 	private static List<GPoint> ans= new ArrayList<GPoint>();
-
 	
+/*	
 	private static Pool<GPoint> pool;
 	public static final int MAX_POOL_SIZE = 120;
 	
@@ -43,14 +44,21 @@ public class Vision
 		};
 		pool = new Pool<GPoint>(factory, MAX_POOL_SIZE);
 	}
+*/
 	
+//	public static List<GPoint> getVision( GPoint locIn, int viewIn, int widthIn, int heightIn, char directionIn)
 	public static List<GPoint> getVision( Map iMAP, Unit iUNIT )
 	{
+//		loc= new GPoint(locIn);
+//		view=viewIn;
+//		width=widthIn;
+//		height=heightIn;
+//		direction=directionIn;
+	
 		MAP=iMAP;
 		UNIT=iUNIT;
 		
-		temp= UNIT.getXYCoordinate();
-		loc= new GPoint(temp.row +1, temp.col +1);
+		loc= UNIT.getXYCoordinate();
 		view=UNIT.getVisionRadius();
 		width = MAP.getNum_horizontal_tiles();
 		height = MAP.getNum_vertical_tiles();
@@ -64,27 +72,28 @@ public class Vision
 	{
 		limit();
 
-		for(int i=ny;i<=py;i++)
-			for(int j=nx;j<=px;j++) {
-				GPoint point = pool.newObject();
+		for(int i=left;i<=right;i++)
+			for(int j=top;j<=bottom;j++) {
+//				GPoint point = pool.newObject();
 				if( (direction==Unit.FACING_UP)||(direction==Unit.FACING_DOWN) ) //up or down
-				{
-					if( Math.abs(loc.row-j) <= Math.abs(loc.col-i) )
-						if( (view+.5) > (Math.sqrt( ((loc.row-j)*(loc.row-j))+((loc.col-i)*(loc.col-i)) )) )
-						{
-							//ans = Arrays.copyOf(ans, ans.length + 1);
-							point.row = j-1; point.col = i-1;
-							ans.add(point);
-						}
-				}
-				else //left or right
+//				if( (direction=='u')||(direction=='d') )
 				{
 					if( Math.abs(loc.row-j) >= Math.abs(loc.col-i) )
 						if( (view+.5) > (Math.sqrt( ((loc.row-j)*(loc.row-j))+((loc.col-i)*(loc.col-i)) )) )
 						{
-							//ans = Arrays.copyOf(ans, ans.length + 1);
-							point.row = j-1; point.col = i-1;
-							ans.add(point);
+//							point.row = j-1; point.col = i-1;
+//							ans.add(point);
+							ans.add(new GPoint(j,i));
+						}
+				}
+				else //left or right
+				{
+					if( Math.abs(loc.row-j) <= Math.abs(loc.col-i) )
+						if( (view+.5) > (Math.sqrt( ((loc.row-j)*(loc.row-j))+((loc.col-i)*(loc.col-i)) )) )
+						{
+//							point.row = j-1; point.col = i-1;
+//							ans.add(point);
+							ans.add(new GPoint(j,i));
 						}
 				}
 			}
@@ -94,77 +103,77 @@ public class Vision
 
 	private static void limit()
 	{
+//		if(direction=='u')
 		if(direction==Unit.FACING_UP)
 		{
-			nx=loc.row-view;
-			if(nx<1)
-				nx=1;
+			left=loc.col-view;
+			if(left<0)
+				left=0;
 
-			px=loc.row+view;
-			if(px>width)
-				px=width;
+			right=loc.col+view;
+			if(right>=width)
+				right=width-1;
 
-			ny=loc.col-view;
-			if(ny<1)
-				ny=1;
+			top=loc.row-view;
+			if(top<0)
+				top=0;
 
-			py=loc.col-1;
-//			if(py<1)
-//				py=1;
+			bottom=loc.row-1;
+
 		}//dir up
+		
+//		else if(direction=='d')
 		else if(direction==Unit.FACING_DOWN)
 		{
-			nx=loc.row-view;
-			if(nx<1)
-				nx=1;
+			left=loc.col-view;
+			if(left<0)
+				left=0;
 
-			px=loc.row+view;
-			if(px>width)
-				px=width;
+			right=loc.col+view;
+			if(right>=width)
+				right=width-1;
 
-			ny=loc.col+1;
-//			if(ny>height)
-//				ny=height;
+			top=loc.row+1;
 
-			py=loc.col+view;
-			if(py>height)
-				py=height;
+			bottom=loc.row+view;
+			if(bottom>=height)
+				bottom=height-1;
 		}//dir down
+		
+//		else if(direction=='l')
 		else if(direction==Unit.FACING_LEFT)
 		{
-			nx=loc.row-view;
-			if(nx<1)
-				nx=1;
+			left=loc.col-view;
+			if(left<0)
+				left=0;
 
-			px=loc.row-1;
-//			if(px<1)
-//				px=1;
+			right=loc.col-1;
 
-			ny=loc.col-view;
-			if(ny<1)
-				ny=1;
+			top=loc.row-view;
+			if(top<0)
+				top=0;
 
-			py=loc.col+view;
-			if(py>height)
-				py=height;
+			bottom=loc.row+view;
+			if(bottom>=height)
+				bottom=height-1;
 		}//dir left
+		
+//		else if(direction=='r')
 		else if(direction==Unit.FACING_RIGHT)
 		{
-			nx=loc.row+1;
-//			if(nx>width)
-//				nx=width;
+			left=loc.col+1;
 
-			px=loc.row+view;
-			if(px>width)
-				px=width;
+			right=loc.col+view;
+			if(right>=width)
+				right=width-1;
 
-			ny=loc.col-view;
-			if(ny<1)
-				ny=1;
+			top=loc.row-view;
+			if(top<0)
+				top=0;
 
-			py=loc.col+view;
-			if(py>height)
-				py=height;
+			bottom=loc.row+view;
+			if(bottom>=height)
+				bottom=height-1;
 		}//dir right
 	}
 
