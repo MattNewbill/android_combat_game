caaf33b8da2e4acca10467ada413e4bb7e652ee4
maxx9 <maxxzero@ymail.com>
2015-02-09 15:10:49 -0800

    added rounded rectangle graphics

--- src/combatgame/graphics/Graphics2D.java
+++ src/combatgame/graphics/Graphics2D.java
@@ -9,6 +9,7 @@ public interface Graphics2D {
 	public void setCanvas(Canvas canvas);
 	public void drawRGB(int r, int g, int b);
 	public void drawRect(float left, float top, float right, float bottom, Paint paint);
+	public void drawRoundRect(float left, float top, float right, float bottom, float rx, float ry, Paint paint);
 	public void drawBitmap(Bitmap bitmap, float left, float top, Paint paint);
 	public void drawText(String text, float x, float y, Paint paint);
 	public int getHeight();
